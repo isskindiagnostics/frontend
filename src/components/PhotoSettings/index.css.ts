5e05e7e73a07696d6c57ef5988d99fe01c738a0a
Beatriz Nonato <bia_nonato@icloud.com>
2025-07-24 18:58:07 -0300

    add photo settings component

---
+++ src/components/PhotoSettings/index.css.ts
@@ -0,0 +1,47 @@
+import { style } from "@vanilla-extract/css";
+import { theme } from "isskinui";
+
+export const container = style({
+  position: "relative",
+  width: 120,
+  height: 120,
+  borderRadius: "50%",
+  overflow: "hidden",
+  cursor: "pointer",
+});
+
+export const profileImage = style({
+  width: "100%",
+  height: "100%",
+  objectFit: "cover",
+  display: "block",
+});
+
+export const editOverlay = style({
+  position: "absolute",
+  top: 0,
+  left: 0,
+  width: "100%",
+  height: "100%",
+  backgroundColor: "rgba(0, 0, 0, 0.5)",
+  opacity: 0,
+  display: "flex",
+  alignItems: "center",
+  justifyContent: "center",
+  transition: "opacity 0.3s ease-in-out",
+  selectors: {
+    [`${container}:hover &`]: {
+      opacity: 1,
+    },
+  },
+});
+
+export const icon = style({
+  color: theme.colors.brandWhite,
+  width: 24,
+  height: 24,
+});
+
+export const inputHidden = style({
+  display: "none",
+});
