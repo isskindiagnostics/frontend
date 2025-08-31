87fa7fb05c60c5ce022805d40dfc5b45badc4874
Larissa Soares <larissa.soares@neugelb.com>
2025-08-31 21:51:37 +0200

    feat: add basic data fetch

--- src/app/subscription/SubscriptionClient/PaymentMethods/index.tsx
+++ src/app/subscription/SubscriptionClient/PaymentMethods/index.tsx
@@ -1,31 +1,39 @@
-import { Card, IconLink } from "isskinui";
+"use client";
+import { Card, Flag, IconLink } from "isskinui";
 
 import ContentBlock from "@/components/ContentBlock";
+import { useUserData } from "@/hooks/useUserData";
 
 import * as styles from "./index.css";
 
 const PaymentMethods = () => {
+  const { userData } = useUserData();
+
+  const cardHolder = userData?.userData.name;
+  const defaultCard = userData?.subscription.stripeData?.savedCards?.find(
+    (card) => card.isDefault
+  );
+
+  const flag = (defaultCard?.brand as Flag) || "visa";
+
   return (
     <ContentBlock>
       <div className={styles.titleWrapper}>
-        <h3>Seu plano</h3>
-        <IconLink icon="Plus">Adicionar novo cartão</IconLink>
+        <h3>Método de Pagamento</h3>
+        {/* <IconLink icon="Plus">Adicionar novo cartão</IconLink> */}
       </div>
 
-      <p>
-        Configure sua assinatura e escolha o plano que melhor atende às suas
-        necessidades.
-      </p>
+      <p>Adicione, remova ou atualize seus métodos de pagamento.</p>
 
       <div className={styles.cards}>
         <Card
-          onClick={() => console.log("TODO")}
-          cardName="Nubank"
-          cardHolder="Julia de Assis"
-          cardNumber="1234"
-          expirationDate={new Date()}
-          isDefault={true}
-          flag="mastercard"
+          // onClick={() => console.log("TODO")}
+          // cardName=""
+          cardHolder={cardHolder || ""}
+          cardNumber={defaultCard?.last4 || ""}
+          expirationDate={new Date(defaultCard?.expYear || "")}
+          isDefault={defaultCard?.isDefault || true}
+          flag={flag}
         />
       </div>
     </ContentBlock>
